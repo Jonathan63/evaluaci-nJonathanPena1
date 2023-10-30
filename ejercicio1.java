4b0952714e8b2af4580f82a7db4fa3d14fa17cf1
Jonathan Peña <67765142+Jonathan63@users.noreply.github.com>
2023-10-30 00:42:31 -0500

    mostrar resultado de la matriz

--- ejercicio1.java
+++ ejercicio1.java
@@ -16,7 +16,27 @@ public class ejercicio1 {
         int fila = scanner.nextInt();
         System.out.print("Ingrese la columna (0-4): ");
         int columna = scanner.nextInt();
+        
+        if (fila >= 0 && fila < 5 && columna >= 0 && columna < 5) {
+            // Ubicar el número en la matriz
+            matriz[fila][columna] = numero;
 
+
+            for (int i = 0; i < 5; i++) {
+                matriz[fila][i] = numero + i - columna;
+                matriz[i][columna] = numero + i - fila;
+            }
+
+            System.out.println("Matriz resultante:");
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    System.out.print(matriz[i][j] + "\t");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("La ubicación está fuera de los límites de la matriz.");
+        }
        
     }
 }
