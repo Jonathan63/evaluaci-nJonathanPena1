cc465234e8f4b549b6365a73e49c774d9f0f7619
Jonathan Peña <67765142+Jonathan63@users.noreply.github.com>
2023-10-30 00:30:33 -0500

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Crear una matriz 5x5
+        int[][] matriz = new int[5][5];
+
+        // Ingresar un número
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+       
+    }
+}
